cac3a37755a9fdc006e3c3bde03b73c60373e3f4
soheilhassanjani <soheilpcmir@gmail.com>
2020-06-08 20:37:01 +0430

    change - 2

--- routes/admin.js
+++ routes/admin.js
@@ -15,6 +15,14 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/add-category => GET
+
+router.get('/add-category', adminController.getAddCategory);
+
+// /admin/add-category => POST
+
+router.post('/add-category', adminController.postAddCategory);
+
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
